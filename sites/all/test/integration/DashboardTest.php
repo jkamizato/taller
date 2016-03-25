2b4fc4387c6cdbbb1e9596a3c3908f09d268c972
Júlio Kamizato <jkamizato@gmail.com>
2016-03-25 15:14:12 -0300

    Test de integracao com selenium

---
+++ sites/all/test/integration/DashboardTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: julio
+ * Date: 3/24/16
+ * Time: 23:59
+ */
+
+include realpath(dirname(__FILE__)) . '/../phpunit_bootstrap.inc';
+
+class DashboardTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        global $base_url;
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl($base_url);
+    }
+
+    public function testAcceptancePageTickets()
+    {
+        $this->url('/tickets');
+
+        $this->assertContains('Bem vindo ao Suporte', $this->byTag('body')->text());
+        $this->assertContains('Abrir um novo ticket', $this->source());
+    }
+
+}
