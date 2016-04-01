5efd3f2eb6589ad623a1e5a17986e36e93bec7da
Júlio Kamizato <jkamizato@gmail.com>
2016-04-01 10:56:56 -0300

    Correcao de tela de resposta

--- sites/all/themes/adminimal_theme/templates/ticket-node-resposta.tpl.php
+++ sites/all/themes/adminimal_theme/templates/ticket-node-resposta.tpl.php
@@ -1,12 +1,16 @@
-<h1>Ticket <?php print $node->title;?></h1>
 <?php
-$categoria = field_view_field('node', $node, 'field_ticket_categoria');
+$language = $node->language;
+kpr($node);?>
+<h1>Ticket <?php print $node->field_ticket_codigo[$language][0]['value'];?></h1>
+<?php
+$categoria = field_view_field('node', $node, 'field_ticket_categoria', array('type' => 'taxonomy_term_reference_plain'));
 print render($categoria);
-$produto = field_view_field('node', $node, 'field_ticket_produto');
+$produto = field_view_field('node', $node, 'field_ticket_produto', array('type' => 'taxonomy_term_reference_plain'));
 print render($produto);
 $date = format_date($node->created, 'medium_brasil');
-print render($date);
+
 ?>
+<strong>Data:</strong> <?php print render($date);?>
 <div style="border: 1px solid">
     <?php $body = field_view_field('node', $node, 'body', array('label' => 'hidden'));
         print render($body);
